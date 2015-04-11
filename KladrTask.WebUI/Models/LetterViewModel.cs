fed625c1b46226800871a54f5afea2d094d10da0
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-04-12 03:30:46 +0500

    Отправка писем админом группе пользователей, начало. (Надо поправить все баги и внешний вид).

---
+++ KladrTask.WebUI/Models/LetterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using KladrTask.Domain.Entities;
+
+namespace KladrTask.WebUI.Models
+{
+    public class LetterViewModel
+    {
+        public string Receiver { get; set; }
+        public string Address { get; set; }
+        public string Index { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ KladrTask.WebUI/Models/LettersListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace KladrTask.WebUI.Models
+{
+    public class LettersListViewModel
+    {
+        public List<LetterViewModel> Letters { get; set; }
+    }
+}
